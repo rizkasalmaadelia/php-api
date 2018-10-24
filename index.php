1eab5c24264cf1916b3b0f41a5784a27962dba90
RIZKA SALMA ADELIA RIZKA SALMA ADELIA <rizkasalmaadelia@gmail.com>
2018-10-24 19:44:52 +0700

    crud inputan

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+  <title>Menampilkan data dari database ke dalam bentuk tabel</title>
+  <link rel="stylesheet" type="text/css" href="edit.css">
+</head>
+<body>
+
+  <div class="container">
+    <h2><center>DATA SISWA</center></h2>
+    <table class=" table table-bordered table-hovered" align="center">
+  <tr>
+    <th>id</th>
+    <th>username</th>
+    <th>password</th>
+    <th>level</th>
+    <th>fullname</th>
+
+  </tr></div>
+  <?php
+  include('koneksi.php');
+  $sql_tampil ="SELECT * FROM kk4";
+  $peserta=mysqli_query($conn,$sql_tampil);
+  while($baris_data=mysqli_fetch_array($peserta,MYSQLI_ASSOC)){
+    echo'
+    </tr>
+      <td>'.$baris_data['id'].'</td>
+      <td>'.$baris_data['username'].'</td>
+      <td>'.$baris_data['password'].'</td>
+      <td>'.$baris_data['level'].'</td>
+      <td>'.$baris_data['fullname'].'</td>
+    </tr>';
+  }
+  ?>
+</body>
+</html>
